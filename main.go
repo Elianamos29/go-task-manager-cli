eeceb1c272b863944321809de843362bd7e4a5b5
Elianamos <elianamos29@gmail.com>
2024-10-11 12:03:59 +0300

    Add task completion feature

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"strconv"
 )
 
 var tasks = []Task{
@@ -12,15 +13,30 @@ var tasks = []Task{
 
 func main() {
 	newTask := flag.String("add", "", "add a task")
+	doneTaskID := flag.String("done", "", "mark task as done")
 	flag.Parse()
 
 	if *newTask != "" {
 		addTask(*newTask)
 	}
 
+	if *doneTaskID != "" {
+		id, err := strconv.Atoi(*doneTaskID)
+		if err == nil {
+			markAsDone(id)
+		} else {
+			fmt.Println("Invalid task id")
+		}
+	}
+
 	fmt.Println("Your tasks:")
 	for _, task := range tasks {
-		fmt.Printf("%d. %s [Done: %t]\n", task.ID, task.Name, task.Done)
+		status := "Not done"
+		if task.Done {
+			status = "Done"
+		}
+
+		fmt.Printf("%d. %s [%s]\n", task.ID, task.Name, status)
 	}
 }
 
@@ -28,4 +44,16 @@ func addTask(name string) {
 	newID := len(tasks) + 1
 	tasks = append(tasks, Task{ID: newID, Name: name, Done: false})
 	fmt.Printf("Added task: %s\n", name)
+}
+
+func markAsDone(id int) {
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks[i].Done = true
+			fmt.Printf("Task %d marked as done\n", id)
+			return
+		}
+	}
+
+	fmt.Println("Task not found")
 }
