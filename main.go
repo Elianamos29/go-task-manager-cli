fa3f878c4dfeb7e92edc951ddefd33ac328b2837
Elianamos <elianamos29@gmail.com>
2024-10-11 11:44:14 +0300

    Add task creation feature

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
+	"flag"
 )
 
+var tasks = []Task{
+	{ID: 1, Name: "Learn Go", Done: false},
+	{ID: 2, Name: "Write cli", Done: false},
+}
+
 func main() {
-	tasks := []Task{
-		{ID: 1, Name: "Learn Go", Done: false},
-		{ID: 2, Name: "Write cli", Done: false},
+	newTask := flag.String("add", "", "add a task")
+	flag.Parse()
+
+	if *newTask != "" {
+		addTask(*newTask)
 	}
 
 	fmt.Println("Your tasks:")
 	for _, task := range tasks {
 		fmt.Printf("%d. %s [Done: %t]\n", task.ID, task.Name, task.Done)
 	}
+}
+
+func addTask(name string) {
+	newID := len(tasks) + 1
+	tasks = append(tasks, Task{ID: newID, Name: name, Done: false})
+	fmt.Printf("Added task: %s\n", name)
 }
